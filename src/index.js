be9dc8ab19c59d5a4bf6b3cdf095c5b08e55dd80
Judex66 <78801062+Judex66@users.noreply.github.com>
2021-03-08 11:22:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 module.exports = function reverse (n) {
-  for(var r = 0; n; n = Math.floor(n / 10)) {
-         r *= 10;
-         r += n % 10;
-     }
-     return r;
+var res = 0;
+if (n<0){
+n=n*(-1);
+}
+  while (n) {
+    res = res * 10 + n % 10;
+    n = Math.floor(n / 10);
+  }
+  return res;
     }
+
